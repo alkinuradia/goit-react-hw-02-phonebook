1a6e19d533b9387e20ec784e5736471229047ee5
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-04-27 11:53:01 +0300

    correct

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,10 +15,7 @@ export class App extends React.Component {
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Serg Dykyy', number: '367-56-78' },
-      { id: 'id-5', name: 'Stefan Sikorsky', number: '227-91-26' },
-      { id: 'id-6', name: 'Maria Better', number: '567-45-92' },
-      { id: 'id-7', name: 'Annie Copeland', number: '896-83-44' },
+      { id: 'id-4', name: 'Annie Copeland', number: '896-83-44' },
     ],
     filter: '',
   };
